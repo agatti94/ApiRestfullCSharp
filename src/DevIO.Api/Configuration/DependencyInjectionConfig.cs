0f26f62331bca27091944a2929a5dffdfb90c204
Arthur <arthurgatti9@gmail.com>
2023-03-08 23:59:11 -0300

    First Commit

---
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,28 @@
+﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using System.Runtime.CompilerServices;
+
+namespace DevIO.Api.Configuration
+{
+    //É recomendado o uso de classes estáticas para manter métodos não associados com um objeto específico.
+    //Elas são caracterizadas por não poderem ser herdadas, já que são seladas.
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,71 @@
+using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //validação de notificações de erros
+
+        //validação de modelstate
+
+        //validação da operação de negócios
+
+        private readonly INotificador _notificador;
+
+        public MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.ObterNotificacoes().Select(n => n.Mensagem)
+            });
+        }
+
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelstate)
+        {
+            if (!modelstate.IsValid) NotificarErroModelInvalida(modelstate);
+            return CustomResponse();
+        }
+
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelstate)
+        {
+            var erros = modelstate.Values.SelectMany(e => e.Errors);
+            foreach (var erro in erros)
+            {
+                var errorMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(errorMsg);
+            }
+        }
+
+        protected void NotificarErro(string mensagem) 
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+    }
+}
