4b483e5296155c9a7e1d6eae7d848667fa03c380
Arthur <arthurgatti9@gmail.com>
2023-03-09 23:01:07 -0300

    Adicionando Upload de imagens

---
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,107 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+        public ProdutosController(INotificador notificador,
+                                  IProdutoService produtoService,
+                                  IProdutoRepository produtoRepository,
+                                  IMapper mapper) : base(notificador)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterTodos());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null) return NotFound();
+
+            return produtoViewModel;
+        }
+
+        [HttpDelete("{id}")]
+        private async Task<ActionResult<ProdutoViewModel>> Excluir(Guid id)
+        {
+            var produto = await ObterProduto(id);
+
+            if (produto == null) return NotFound();
+
+            await _produtoService.Remover(id);
+
+            return CustomResponse(produto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var imagemNome = Guid.NewGuid() + "_" + produtoViewModel.Imagem;
+            if (!UploadArquivo(produtoViewModel.ImagemUpload, imagemNome))
+            {
+                return CustomResponse(produtoViewModel);
+            }
+
+            produtoViewModel.Imagem = imagemNome;
+
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return CustomResponse(produtoViewModel);
+        }
+
+        //recebe o arquivo em base64 e o nome da imagem
+        private bool UploadArquivo(string arquivo, string imgNome)
+        {
+            //Coleçao de bytes que vai ser convertido de base64 para string
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
+
+            //verifico se o arquivo se a coleção da string vem nula ou se o tamanho é igual a zero adiciono um erro na modelstate
+            if(string.IsNullOrEmpty(arquivo))
+            {
+                NotificarErro("Forneça uma imagem para este produto!");
+                return false;
+            }
+
+            //pego o diretorio raiz que estou e combino para formar o path do arquivo
+
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
+
+            if(System.IO.File.Exists(filepath))
+            {
+                ModelState.AddModelError(string.Empty, "Já existe um arquivo com este nome");
+                return false;
+            }
+
+            //vou gerar o arquivo passando o filepath e a coleção de bytes
+            System.IO.File.WriteAllBytes(filepath, imageDataByteArray);
+
+            return true;
+        }
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid id)
+        {
+            var produto = _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutoFornecedor(id));
+            return produto;
+        }
+    }
+}
